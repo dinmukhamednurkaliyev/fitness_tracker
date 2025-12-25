67f852641b68b62c0aa9bd8b7913803dda362aa5
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-12-25 05:32:06 +0500

    chore: initial project scaffolding based on Tutorial (#8)

--- lib/l10n/l10n.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fitness_tracker_application/l10n/gen/app_localizations.dart';
-import 'package:flutter/widgets.dart';
-
-export 'package:fitness_tracker_application/l10n/gen/app_localizations.dart';
-
-extension AppLocalizationsX on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this);
-}
